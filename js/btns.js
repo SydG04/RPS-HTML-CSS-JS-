a15c59cd524cbeed6ea219570ce224b4c9b27abc
SydG04 <sgilmore@students.pccc.edu>
2023-12-24 18:03:52 -0500

    Starting a new project. Practicing JS

---
+++ js/btns.js
@@ -0,0 +1,14 @@
+btncontainer = document.getElementsByClassName("yn-btns");
+yesBtn = document.getElementById("yes");
+noBtn = document.getElementById("no");
+
+//event listener
+
+//functions
+/*
+1. Set timer to show the yn container 
+*/
+
+function showBTNS {
+  
+}
